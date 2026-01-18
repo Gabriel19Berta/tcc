c8c048411c0b63e2b8b5d650a57bcf3e3370884a
gabrielgberta <130099907+Gabriel19Berta@users.noreply.github.com>
2026-01-18 15:51:31 -0300

    Ajusta estilo na tela de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,53 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                /* Primária */
+                primary: {
+                    DEFAULT: '#7F8F5E',
+                    600: '#6e7d52',
+                    700: '#5f6d47',
+                },
+
+                /* Texto */
+                text: {
+                    DEFAULT: '#4C4C4C',
+                    light: '#6B6B6B',
+                },
+
+                /* Cinzas */
+                gray: {
+                    700: '#4C4C4C',
+                    400: '#BABABA',
+                    300: '#B4B4B4',
+                },
+
+                /* Backgrounds */
+
+                background: {
+                    soft: '#F6F6F6',
+                    white: '#FFFFFF',
+                },
+
+                /* Estados */
+                danger: '#F05143',
+                info: '#437DF0',
+
+                /* Neutra */
+                beige: '#C7AF94',
+            }
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        function ({ addUtilities }) {
+            addUtilities({
+                    '.required::after': {
+                    content: '" *"',
+                    color: '#F05143',
+                },
+            })
+        }
+    ],
 };
