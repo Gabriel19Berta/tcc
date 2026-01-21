ebb378b344e22b0d9d98f5aa066c06cafe935625
gabrielgberta <130099907+Gabriel19Berta@users.noreply.github.com>
2026-01-20 21:18:45 -0300

    Adicione dropdown de pessoas no menu de navegação

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,38 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <!-- Dropdown Pessoas -->
+                    <div class="inline-flex items-center">
+                        <x-dropdown align="left" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md
+                                        text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition"
+                                >
+                                    <div>Pessoas</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link {{-- :href="route('clientes.index')" --}}>
+                                    {{ __('Clientes') }}
+                                </x-dropdown-link>
+
+                                <x-dropdown-link {{-- :href="route('funcionarios.index')" --}}>
+                                    {{ __('Funcionários') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 </div>
             </div>
 
@@ -98,3 +130,5 @@
         </div>
     </div>
 </nav>
+
+<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
