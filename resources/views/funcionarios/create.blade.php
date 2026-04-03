be65d4435c1c7768a61e634dae266e508f8fa3d2
gabrielgberta <130099907+Gabriel19Berta@users.noreply.github.com>
2026-04-03 11:20:19 -0300

    Refataração de uso de classes em clientes e funcionarios

--- resources/views/funcionarios/create.blade.php
+++ resources/views/funcionarios/create.blade.php
@@ -16,7 +16,7 @@
                         <div class="grid grid-cols-1 md:grid-cols-5 gap-4">
                             <div class="md:col-span-2">
                                 <x-input-label for="nome" :value="__('Nome')" class="required" />
-                                <x-text-input id="nome" name="nome" type="text" class="block mt-1 w-full"
+                                <x-text-input id="nome" name="nome" type="text" class="w-full"
                                     :value="old('nome')" />
                             </div>
                             <div class="md:col-span-1">
@@ -39,32 +39,32 @@
                             <div id="cpf-field">
                                 <x-input-label for="cpf" :value="__('CPF')" />
                                 <x-text-input id="cpf" name="cpf" type="text"
-                                    class="block mt-1 w-full mask-cpf" :value="old('cpf')" />
+                                    class="w-full mask-cpf" :value="old('cpf')" />
                             </div>
                             <div id="cnpj-field" class="hidden">
                                 <x-input-label for="cnpj" :value="__('CNPJ')" />
                                 <x-text-input id="cnpj" name="cnpj" type="text"
-                                    class="block mt-1 w-full mask-cnpj" :value="old('cnpj')" />
+                                    class="w-full mask-cnpj" :value="old('cnpj')" />
                             </div>
                             <div id="rg-field">
                                 <x-input-label for="rg" :value="__('RG')" />
-                                <x-text-input id="rg" name="rg" type="text" class="block mt-1 w-full"
+                                <x-text-input id="rg" name="rg" type="text" class="w-full"
                                     :value="old('rg')" />
                             </div>
                             <div id="ie-field" class="hidden">
                                 <x-input-label for="ie" :value="__('Inscrição Estadual')" />
-                                <x-text-input id="ie" name="ie" type="text" class="block mt-1 w-full"
+                                <x-text-input id="ie" name="ie" type="text" class="w-full"
                                     :value="old('ie')" />
                             </div>
                             <div id="data-nascimento">
                                 <x-input-label for="data_nascimento" :value="__('Data nascimento')" />
                                 <x-text-input id="data_nascimento" name="data_nascimento" type="date"
-                                    class="block mt-1 w-full" :value="old('data_nascimento')" />
+                                    class="w-full" :value="old('data_nascimento')" />
                             </div>
                             <div>
                                 <x-input-label for="data_admissao" :value=" __('Data admissão')"/>
                                 <x-text-input id="data_admissao" name="data_admissao" type="date"
-                                    class="block mt-1 w-full" :value="old('data_admissao')"/>
+                                    class="w-full" :value="old('data_admissao')"/>
                             </div>
 
                             <h2 class="md:col-span-5 block text-primary font-semibold text-xl leading-tight mt-8">
@@ -73,17 +73,17 @@
                             <div>
                                 <x-input-label for="celular" :value="__('Celular')" />
                                 <x-text-input id="celular" name="celular" type="text"
-                                    class="block mt-1 w-full mask-celular" :value="old('celular')" />
+                                    class="w-full mask-celular" :value="old('celular')" />
                             </div>
                             <div>
                                 <x-input-label for="telefone" :value="__('Telefone')" />
                                 <x-text-input id="telefone" name="telefone" type="text"
-                                    class="block mt-1 w-full mask-telefone" :value="old('telefone')" />
+                                    class="w-full mask-telefone" :value="old('telefone')" />
                             </div>
 
                             <div class="md:col-span-2">
                                 <x-input-label for="email" :value="__('Email')" />
-                                <x-text-input id="email" name="email" type="email" class="block mt-1 w-full"
+                                <x-text-input id="email" name="email" type="email" class="w-full"
                                     :value="old('email')" />
                             </div>
 
@@ -94,31 +94,31 @@
                             <div>
                                 <x-input-label for="cep" :value="__('CEP')" />
                                 <x-text-input id="cep" name="cep" type="text"
-                                    class="block mt-1 w-full mask-cep" :value="old('cep')" />
+                                    class="w-full mask-cep" :value="old('cep')" />
                             </div>
                             <div class="md:col-span-2">
                                 <x-input-label for="logradouro" :value="__('Logradouro')" />
-                                <x-text-input id="logradouro" name="logradouro" type="text" class="block mt-1 w-full"
+                                <x-text-input id="logradouro" name="logradouro" type="text" class="w-full"
                                     :value="old('logradouro')" />
                             </div>
                             <div>
                                 <x-input-label for="numero" :value="__('Número')" />
-                                <x-text-input id="numero" name="numero" type="text" class="block mt-1 w-full"
+                                <x-text-input id="numero" name="numero" type="text" class="w-full"
                                     :value="old('numero')" />
                             </div>
                             <div class="md:col-span-2">
                                 <x-input-label for="complemento" :value="__('Complemento')" />
                                 <x-text-input id="complemento" name="complemento" type="text"
-                                    class="block mt-1 w-full" :value="old('complemento')" />
+                                    class="w-full" :value="old('complemento')" />
                             </div>
                             <div class="md:col-span-2">
                                 <x-input-label for="bairro" :value="__('Bairro')" />
-                                <x-text-input id="bairro" name="bairro" type="text" class="block mt-1 w-full"
+                                <x-text-input id="bairro" name="bairro" type="text" class="w-full"
                                     :value="old('bairro')" />
                             </div>
                             <div class="md:col-span-2">
                                 <x-input-label for="cidade" :value="__('Cidade')" />
-                                <x-text-input id="cidade" name="cidade" type="text" class="block mt-1 w-full"
+                                <x-text-input id="cidade" name="cidade" type="text" class="w-full"
                                     :value="old('cidade')" />
                             </div>
                             <div class="md:col-span-1">
