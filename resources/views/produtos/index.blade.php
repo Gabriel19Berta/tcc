9408b1e8564654b10be90f4b1e5b7ee615a15ca3
gabrielgberta <130099907+Gabriel19Berta@users.noreply.github.com>
2026-06-06 21:12:36 -0300

    refactor(produto): padroniza formulário de filtro

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -14,58 +14,48 @@
     </x-slot>
         
     {{-- FILTROS --}}
-    <form method="GET" action="{{ route('produtos.index') }}" class="mb-4 flex flex-col lg:flex-row lg:justify-between gap-4 sm:px-0 px-4">
-        <div class="flex flex-wrap gap-2 items-end">
-            <div>
-                <x-input-label for="codigo" :value="__('Código')" />
-                <x-text-input id="codigo" name="codigo" type="number" min="0" :value="request('codigo')" class="w-24" />
-            </div>
-            <div>
-                <x-input-label for="status" :value="__('Status')" />
-                <select id="status" name="status">
-                    <option value="todos" {{ request('status') === 'todos' ? 'selected' : '' }}>Todos</option>
-                    <option value="1" {{ request('status', '1') === '1' ? 'selected' : '' }}>Ativo</option>
-                    <option value="0" {{ request('status') === '0' ? 'selected' : '' }}>Inativo</option>
-                </select>
-            </div>
-            <div>
-                <x-input-label for="nome" :value="__('Nome')" />
-                <x-text-input id="nome" name="nome" type="text" :value="request('nome')" class="w-96"/>
-            </div>
-            <div class="w-96">
-                <x-input-label for="nome" :value="__('Marca')" />
-                <select name="marca_id" id="marca_id" class="select2">
-                    <option value=""></option>
-                    @foreach ($marcas as $marca)
-                        <option value="{{ $marca['id'] }}"
-                            {{ request()->get('marca_id') == $marca['id'] ? 'selected' : '' }}>
-                            {{ $marca['nome'] }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="w-96">
-                <x-input-label for="nome" :value="__('Tipo de produto')" />
-                <select name="tipo_produto_id" id="tipo_produto_id" class="select2">
-                    <option value=""></option>
-                    @foreach ($tipo_produtos as $tipo_produto)
-                        <option value="{{ $tipo_produto['id'] }}"
-                        {{ request()->get('tipo_produto_id') == $tipo_produto['id'] ? 'selected' : '' }}>
-                        {{ $tipo_produto['nome'] }}
+    <x-filter-form :action="route('produtos.index')">
+        <div>
+            <x-input-label for="codigo" :value="__('Código')" />
+            <x-text-input id="codigo" name="codigo" type="number" min="0" :value="request('codigo')" class="w-24" />
+        </div>
+        <div>
+            <x-input-label for="status" :value="__('Status')" />
+            <select id="status" name="status">
+                <option value="todos" {{ request('status') === 'todos' ? 'selected' : '' }}>Todos</option>
+                <option value="1" {{ request('status', '1') === '1' ? 'selected' : '' }}>Ativo</option>
+                <option value="0" {{ request('status') === '0' ? 'selected' : '' }}>Inativo</option>
+            </select>
+        </div>
+        <div>
+            <x-input-label for="nome" :value="__('Nome')" />
+            <x-text-input id="nome" name="nome" type="text" :value="request('nome')" class="w-96"/>
+        </div>
+        <div class="w-96">
+            <x-input-label for="nome" :value="__('Marca')" />
+            <select name="marca_id" id="marca_id" class="select2">
+                <option value=""></option>
+                @foreach ($marcas as $marca)
+                    <option value="{{ $marca['id'] }}"
+                        {{ request()->get('marca_id') == $marca['id'] ? 'selected' : '' }}>
+                        {{ $marca['nome'] }}
                     </option>
-                    @endforeach
-                </select>
-            </div>
+                @endforeach
+            </select>
         </div>
-        <div class="flex flex-col sm:flex-row gap-2 sm:items-end">
-            <x-limpar-filtro class="w-full sm:w-auto"/>
-
-            <x-primary-button class="w-full sm:w-auto justify-center">
-                {{ __('Buscar') }}
-                <i class="fa-solid fa-magnifying-glass ml-2"></i>
-            </x-primary-button>
+        <div class="w-96">
+            <x-input-label for="nome" :value="__('Tipo de produto')" />
+            <select name="tipo_produto_id" id="tipo_produto_id" class="select2">
+                <option value=""></option>
+                @foreach ($tipo_produtos as $tipo_produto)
+                    <option value="{{ $tipo_produto['id'] }}"
+                    {{ request()->get('tipo_produto_id') == $tipo_produto['id'] ? 'selected' : '' }}>
+                    {{ $tipo_produto['nome'] }}
+                </option>
+                @endforeach
+            </select>
         </div>
-    </form>
+    </x-filter-form>
 
     <table>
         <thead>
