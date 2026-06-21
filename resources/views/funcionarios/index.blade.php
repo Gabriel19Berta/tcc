598e4d3d3782147188409f74990044aa2458d624
gabrielgberta <130099907+Gabriel19Berta@users.noreply.github.com>
2026-06-21 10:54:34 -0300

    fix(funcionários): corrige filtro da index de funcionário

--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -11,42 +11,32 @@
     </x-slot>
    
     {{-- FILTROS --}}
-    <form method="GET" action="{{ route('funcionarios.index') }}" class="mb-4 flex flex-col lg:flex-row lg:justify-between gap-4 sm:px-0 px-4">
-        <div class="flex flex-wrap gap-2 items-end">
-            <div>
-                <x-input-label for="codigo" :value="__('Código')" />
-                <x-input id="codigo" name="codigo" type="number" min="0" :value="request('codigo')" class="w-24" />
-            </div>
-            <div>
-                <x-input-label for="status" :value="__('Status')" />
-                <select id="status" name="status">
-                    <option value="todos" {{ request('status') === 'todos' ? 'selected' : '' }}>Todos</option>
-                    <option value="1" {{ request('status', '1') === '1' ? 'selected' : '' }}>Ativo</option>
-                    <option value="0" {{ request('status') === '0' ? 'selected' : '' }}>Inativo</option>
-                </select>
-            </div>
-            <div>
-                <x-input-label for="nome" :value="__('Nome')" />
-                <x-input id="nome" name="nome" type="text" :value="request('nome')" class="w-96" placeholder="Digite o nome, cpf ou cnpj"/>
-            </div>
-            <div>
-                <x-input-label for="tipo" :value="__('Tipo')" />
-                <select id="tipo" name="tipo">
-                    <option value="" {{ request('tipo') === null ? 'selected' : '' }}>Todos</option>
-                    <option value="f" {{ request('tipo') === 'f' ? 'selected' : '' }}>Física</option>
-                    <option value="j" {{ request('tipo') === 'j' ? 'selected' : '' }}>Jurídica</option>
-                </select>
-            </div>
+    <x-filter-form :action="route('funcionarios.index')">
+        <div>
+            <x-input-label for="codigo" :value="__('Código')" />
+            <x-input id="codigo" name="codigo" type="number" min="0" :value="request('codigo')" class="w-24" />
         </div>
-        <div class="flex flex-col sm:flex-row gap-2 sm:items-end">
-            <x-limpar-filtro class="w-full sm:w-auto"/>
-
-            <x-primary-button class="w-full sm:w-auto justify-center">
-                {{ __('Buscar') }}
-                <i class="fa-solid fa-magnifying-glass ml-2"></i>
-            </x-primary-button>
+        <div>
+            <x-input-label for="status" :value="__('Status')" />
+            <select id="status" name="status">
+                <option value="todos" {{ request('status') === 'todos' ? 'selected' : '' }}>Todos</option>
+                <option value="1" {{ request('status', '1') === '1' ? 'selected' : '' }}>Ativo</option>
+                <option value="0" {{ request('status') === '0' ? 'selected' : '' }}>Inativo</option>
+            </select>
         </div>
-    </form>
+        <div>
+            <x-input-label for="nome" :value="__('Nome')" />
+            <x-input id="nome" name="nome" type="text" :value="request('nome')" class="w-96" placeholder="Digite o nome, cpf ou cnpj"/>
+        </div>
+        <div>
+            <x-input-label for="tipo" :value="__('Tipo')" />
+            <select id="tipo" name="tipo">
+                <option value="" {{ request('tipo') === null ? 'selected' : '' }}>Todos</option>
+                <option value="f" {{ request('tipo') === 'f' ? 'selected' : '' }}>Física</option>
+                <option value="j" {{ request('tipo') === 'j' ? 'selected' : '' }}>Jurídica</option>
+            </select>
+        </div>
+    </x-filter-form>
 
     <table>
         <thead>
