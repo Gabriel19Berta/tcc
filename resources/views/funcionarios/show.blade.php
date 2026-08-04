4e4fb0938be74042741e413c9b0a36664f16d0c7
Gabriel <gabriel19berta@gmail.com>
2026-08-03 21:34:47 -0300

    refactor(shows): Remove tag HTML desnecessária

--- resources/views/funcionarios/show.blade.php
+++ resources/views/funcionarios/show.blade.php
@@ -14,144 +14,142 @@
     <div class="py-4">
         <div class="bg-white rounded-lg overflow-hidden">
             <x-show-table>
-                <tbody>
-                    <tr class="bg-gray-50">
-                        <th colspan="2" scope="colgroup" class="text-base text-primary">
-                            Dados Básicos
-                        </th>
+                <tr class="bg-gray-50">
+                    <th colspan="2" scope="colgroup" class="text-base text-primary">
+                        Dados Básicos
+                    </th>
+                </tr>
+                <tr>
+                    <th>Código</th>
+                    <td>{{ $funcionario->funcionario->id }}</td>
+                </tr>
+                <tr>
+                    <th>Status</th>
+                    <td>{{ $funcionario->status_formatada }}</td>
+                </tr>
+                <tr>
+                    <th>Nome</th>
+                    <td>{{ $funcionario->nome }}</td>
+                </tr>
+
+                @if($funcionario->tipo == 'f')
+                    <tr>
+                        <th>Tipo</th>
+                        <td>{{ __('Física') }}</td>
                     </tr>
                     <tr>
-                        <th>Código</th>
-                        <td>{{ $funcionario->funcionario->id }}</td>
-                    </tr>
-                    <tr>
-                        <th>Status</th>
-                        <td>{{ $funcionario->status_formatada }}</td>
-                    </tr>
-                    <tr>
-                        <th>Nome</th>
-                        <td>{{ $funcionario->nome }}</td>
+                        <th>CPF</th>
+                        <td>{{ $funcionario->cpf }}</td>
                     </tr>
 
-                    @if($funcionario->tipo == 'f')
-                        <tr>
-                            <th>Tipo</th>
-                            <td>{{ __('Física') }}</td>
-                        </tr>
-                        <tr>
-                            <th>CPF</th>
-                            <td>{{ $funcionario->cpf }}</td>
-                        </tr>
+                    <tr>
+                        <th>RG</th>
+                        <td>{{ $funcionario->rg }}</td>
+                    </tr>
 
-                        <tr>
-                            <th>RG</th>
-                            <td>{{ $funcionario->rg }}</td>
-                        </tr>
+                    <tr>
+                        <th>Data Nascimento</th>
+                        <td>{{ $funcionario->data_nascimento_formatada }}</td>
+                    </tr>
+                @elseif ($funcionario->tipo == 'j')
+                    <tr>
+                        <th>Tipo</th>
+                        <td>{{ __('Jurídica') }}</td>
+                    </tr>
+                    <tr>
+                        <th>CNPJ</th>
+                        <td>{{ $funcionario->cnpj }}</td>
+                    </tr>
 
-                        <tr>
-                            <th>Data Nascimento</th>
-                            <td>{{ $funcionario->data_nascimento_formatada }}</td>
-                        </tr>
-                    @elseif ($funcionario->tipo == 'j')
-                        <tr>
-                            <th>Tipo</th>
-                            <td>{{ __('Jurídica') }}</td>
-                        </tr>
-                        <tr>
-                            <th>CNPJ</th>
-                            <td>{{ $funcionario->cnpj }}</td>
-                        </tr>
+                    <tr>
+                        <th>Inscrição Estadual</th>
+                        <td>{{ $funcionario->ie }}</td>
+                    </tr>
+                @else
+                    <tr>
+                        <th>Tipo</th>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th>CPF</th>
+                        <td>{{ $funcionario->cpf }}</td>
+                    </tr>
 
-                        <tr>
-                            <th>Inscrição Estadual</th>
-                            <td>{{ $funcionario->ie }}</td>
-                        </tr>
-                    @else
-                        <tr>
-                            <th>Tipo</th>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <th>CPF</th>
-                            <td>{{ $funcionario->cpf }}</td>
-                        </tr>
+                    <tr>
+                        <th>RG</th>
+                        <td>{{ $funcionario->rg }}</td>
+                    </tr>
 
-                        <tr>
-                            <th>RG</th>
-                            <td>{{ $funcionario->rg }}</td>
-                        </tr>
-
-                        <tr>
-                            <th>Data Nascimento</th>
-                            <td>{{ $funcionario->data_nascimento_formatada }}</td>
-                        </tr>
-                    @endif
                     <tr>
-                        <th>Data Admissão</th>
-                        @php
-                        @endphp
-                        <td>{{ $funcionario->funcionario->data_admissao_formatada }}</td>
+                        <th>Data Nascimento</th>
+                        <td>{{ $funcionario->data_nascimento_formatada }}</td>
                     </tr>
-                    <tr class="bg-gray-50">
-                        <th colspan="2" scope="colgroup" class="text-base text-primary">
-                            Contato
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>Celular</th>
-                        <td>{{ $funcionario->celular }}</td>
-                    </tr>
-                    <tr>
-                        <th>Telefone</th>
-                        <td>{{ $funcionario->telefone }}</td>
-                    </tr>
-                    <tr>
-                        <th>Email</th>
-                        <td>{{ $funcionario->email }}</td>
-                    </tr>
-                    <tr class="bg-gray-50">
-                        <th colspan="2" scope="colgroup" class="text-base text-primary">
-                            Endereço
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>CEP</th>
-                        <td>{{ $funcionario->cep }}</td>
-                    </tr>
-                    <tr>
-                        <th>Logradouro</th>
-                        <td>{{ $funcionario->logradouro }}</td>
-                    </tr>
-                    <tr>
-                        <th>Número</th>
-                        <td>{{ $funcionario->numero }}</td>
-                    </tr>
-                    <tr>
-                        <th>Complemento</th>
-                        <td>{{ $funcionario->complemento }}</td>
-                    </tr>
-                    <tr>
-                        <th>Bairro</th>
-                        <td>{{ $funcionario->bairro }}</td>
-                    </tr>
-                    <tr>
-                        <th>Cidade</th>
-                        <td>{{ $funcionario->cidade }}</td>
-                    </tr>
-                    <tr>
-                        <th>UF</th>
-                        <td>{{ $funcionario->uf }}</td>
-                    </tr>
-                    <tr class="bg-gray-50">
-                        <th colspan="2" scope="colgroup" class="text-base text-primary">
-                            Dados adicionais
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>Observações</th>
-                        <td>{{ $funcionario->funcionario->observacoes }}</td>
-                    </tr>
-                </tbody>
+                @endif
+                <tr>
+                    <th>Data Admissão</th>
+                    @php
+                    @endphp
+                    <td>{{ $funcionario->funcionario->data_admissao_formatada }}</td>
+                </tr>
+                <tr class="bg-gray-50">
+                    <th colspan="2" scope="colgroup" class="text-base text-primary">
+                        Contato
+                    </th>
+                </tr>
+                <tr>
+                    <th>Celular</th>
+                    <td>{{ $funcionario->celular }}</td>
+                </tr>
+                <tr>
+                    <th>Telefone</th>
+                    <td>{{ $funcionario->telefone }}</td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td>{{ $funcionario->email }}</td>
+                </tr>
+                <tr class="bg-gray-50">
+                    <th colspan="2" scope="colgroup" class="text-base text-primary">
+                        Endereço
+                    </th>
+                </tr>
+                <tr>
+                    <th>CEP</th>
+                    <td>{{ $funcionario->cep }}</td>
+                </tr>
+                <tr>
+                    <th>Logradouro</th>
+                    <td>{{ $funcionario->logradouro }}</td>
+                </tr>
+                <tr>
+                    <th>Número</th>
+                    <td>{{ $funcionario->numero }}</td>
+                </tr>
+                <tr>
+                    <th>Complemento</th>
+                    <td>{{ $funcionario->complemento }}</td>
+                </tr>
+                <tr>
+                    <th>Bairro</th>
+                    <td>{{ $funcionario->bairro }}</td>
+                </tr>
+                <tr>
+                    <th>Cidade</th>
+                    <td>{{ $funcionario->cidade }}</td>
+                </tr>
+                <tr>
+                    <th>UF</th>
+                    <td>{{ $funcionario->uf }}</td>
+                </tr>
+                <tr class="bg-gray-50">
+                    <th colspan="2" scope="colgroup" class="text-base text-primary">
+                        Dados adicionais
+                    </th>
+                </tr>
+                <tr>
+                    <th>Observações</th>
+                    <td>{{ $funcionario->funcionario->observacoes }}</td>
+                </tr>
             </x-show-table>
         </div>
     </div>
--- resources/views/marcas/show.blade.php
+++ resources/views/marcas/show.blade.php
@@ -14,25 +14,23 @@
     <div class="py-4">
         <div class="bg-white rounded-lg overflow-hidden">
             <x-show-table>
-                <tbody>
-                    <tr class="bg-gray-50">
-                        <th colspan="2" scope="colgroup" class="text-base text-primary">
-                            Dados Básicos
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>Código</th>
-                        <td>{{ $marca->id }}</td>
-                    </tr>
-                    <tr>
-                        <th>Status</th>
-                        <td>{{ $marca->status_formatada }}</td>
-                    </tr>
-                    <tr>
-                        <th>Nome</th>
-                        <td>{{ $marca->nome }}</td>
-                    </tr>
-                </tbody>
+                <tr class="bg-gray-50">
+                    <th colspan="2" scope="colgroup" class="text-base text-primary">
+                        Dados Básicos
+                    </th>
+                </tr>
+                <tr>
+                    <th>Código</th>
+                    <td>{{ $marca->id }}</td>
+                </tr>
+                <tr>
+                    <th>Status</th>
+                    <td>{{ $marca->status_formatada }}</td>
+                </tr>
+                <tr>
+                    <th>Nome</th>
+                    <td>{{ $marca->nome }}</td>
+                </tr>
             </x-show-table>
         </div>
     </div>
